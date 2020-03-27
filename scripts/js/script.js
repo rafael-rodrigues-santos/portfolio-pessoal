0d22c8f222537739d6d3fb49bf7e64a2aa6eaa82
Rafael <rafaelrodrigues_@outlook.com>
2020-03-27 15:30:49 -0300

    Teste jquery

---
+++ scripts/js/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    $(".teste").onclick(function(){
+        //Aqui estau retirando o conteudo da propriedade href “o link destino” e colocando em uma variavel
+        var link = $(this).attr('href');
+        $.ajax
+        ({
+            dataType: 'html',
+            url: home.html, //link da pagina que o ajax buscará
+            success: function(data)
+            {
+                $(".projeto").html(data).fadeIn(340); //Inserindo o retorno da pagina ajax
+            },
+            error: function(data){
+                $(".projeto").html("<center><p>ERRO ao carregar outra pagina</p></center>").fadeIn(300); //Em caso de erro ele exibe esta mensagem
+            }
+        }); 
+     
+    });
+});
